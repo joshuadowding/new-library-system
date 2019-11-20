ae78888db4b03aa2c386d9e74fa4bd489b661758
Joshua Dowding <joshuadowding@outlook.com>
2019-11-20 18:44:18 +0000

    nls: minor fix to filter page layout

--- NLS/wwwroot/js/site.js
+++ NLS/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $("#clear-filters").click(function () {
+        $("#Author").val("");
+        $("#Publisher").val("");
+        $("#Series").val("");
+        $("#Type").val("");
+        $("#Genre").val("");
+        $("#Form").val("");
+        $("#Age").val("");
+        $("#Location").val("");
+    });
+});
